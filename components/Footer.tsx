16a59d95103748ac58e9ce1e4ee2373f3a211e55
buriihenry <buriihenry@gmail.com>
2025-06-22 03:32:44 +0300

    Enhance Footer and Logo components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,24 +3,24 @@ import Container from './Container'
 import FooterTop from './FooterTop'
 import Logo from './Logo'
 import SocialMedia from './SocialMedia'
+import { SubText } from './ui/text'
 
 function Footer() {
   return (
     <footer className='bg-white border-t'>
         <Container>
           <FooterTop />
-          <div>
-            <div>
+          <div className='py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+            <div className='space-y-4'>
               <Logo />
-              <p>
+              <SubText className='text-gray-600 text-sm'>
                 We are dedicated to providing you with the best online shopping experience. 
-                Our team works tirelessly to ensure that you have access to a wide range of high-quality products at competitive prices.
                 Thank you for choosing us for your shopping needs! 
-              </p>
-              <SocialMedia className='text-darkColor/60'
-                iconClassName='border-darkColor/60 hover:text-primary hover:border-primary transition-colors'
-                tooltipClassName='bg-darkColor text-white' 
-
+              </SubText>
+             <SocialMedia
+              className="text-darkColor/60"
+              iconClassName="border-darkColor/60 hover:border-tech_blue hover:text-tech_blue"
+              
               />
             </div>
             <div></div>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = ({className, spanDesign}: {
   spanDesign?:string
 }) => {
   return (
-    <Link href={"/"}>
+    <Link href={"/"} className='inline-flex'>
         <h2 className={cn("text-2xl text-tech_dark_green font-black tracking-wide uppercase hover:text-tech_light_green hoverEffect group font-sans",className)}>
             Buybudd<span className={cn('text-tech_light_green group-hover:text-tech_dark_green hoverEffect', spanDesign)}>y</span>
         </h2>
