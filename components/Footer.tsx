ce754f8a399fb43c8348113af977086d5994ecb2
buriihenry <buriihenry@gmail.com>
2025-06-22 10:48:17 +0300

    integrate Sanity CMS

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,11 @@ import Container from './Container'
 import FooterTop from './FooterTop'
 import Logo from './Logo'
 import SocialMedia from './SocialMedia'
-import { SubText } from './ui/text'
+import { SubText, SubTitle } from './ui/text'
+import { categoriesData, quickLinksData } from '@/constants/data'
+import Link from 'next/link'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 function Footer() {
   return (
@@ -23,10 +27,55 @@ function Footer() {
               
               />
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <SubTitle> Quick Links</SubTitle>
+              <ul className='space-y-3 mt-4'>
+                {quickLinksData?.map((item)=>(
+                  <li key={item?.title}>
+                    <Link href={item?.href}
+                      className='hover:text-tech_blue hoverEffect font-medium'
+                    >
+                    {item?.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <SubTitle>Categories</SubTitle>
+              <ul className='space-y-3 mt-4'>
+                {categoriesData?.map((item)=>(
+                  <li key={item?.title}>
+                    <Link 
+                    href={`/category/${item?.href}`}
+                    className='hover:text-tech_blue hoverEffect font-medium'
+                    >
+                    {item?.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            
+            <div className='space-y-4'>
+              <SubTitle>Newsletter</SubTitle>
+              <SubText>
+                Subscribe to our newsletter to get the latest updates and offers.
+              </SubText>
+              <form className='space-y-3'>
+                <Input placeholder='Enter your email address' type="email" className='mt-4' required />
+                <Button className='w-full'>Subscribe</Button>
+              </form>
+              
+            </div>
           </div>
+                <div className="py-6 border-t text-center text-sm text-gray-600 items-center flex flex-col gap-2">
+                <div>
+                  © {new Date().getFullYear()} <Logo className="text-sm" />. All
+                  rights reserved.
+                </div>
+              </div>
+          
         </Container>
       
     </footer>
